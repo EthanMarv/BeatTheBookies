1d027c5f1680e4e4fb0c4585fd92e502623dc5bc
Ethan Marvin <P2601167@my365.dmu.ac.uk>
2023-03-12 23:09:20 +0000

    balance working

--- app/src/main/java/com/example/beatthebookies/CoinFlipGame.kt
+++ app/src/main/java/com/example/beatthebookies/CoinFlipGame.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.icu.number.IntegerWidth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Radio
+import android.text.Editable
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.RadioButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.text.set
 
 class CoinFlipGame : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,18 +22,18 @@ class CoinFlipGame : AppCompatActivity() {
         onTap()
     }
 
-    private var balancE : Int = 2000
+    private var balancE : Int = 1000 //temporary
 
     private var HeadStreak : Int = 0
     private var TailStreak : Int = 0
     private var HNextChance : Int = 2
     private var TNextChance : Int = 2
 
-
     fun GoToCFHistory(view: View){
         val intent = Intent(this, CoinFlipHistoryPage::class.java)
         startActivity(intent)
     }
+
 
     private fun onTap() {
 
@@ -38,12 +41,16 @@ class CoinFlipGame : AppCompatActivity() {
 
 
         iv_coin.setOnClickListener {
-            val result = (1..2).random()  // try && IF HEADS.ischecked()
+            val result = (1..2).random()
             if (result == 1) {
 
                 val newHeads = CoinFlipHistory(-1,"Heads")
                 val mydatabase = DataBaseHelper(this)
                 val result = mydatabase.addCoinFlipResult(newHeads)
+                val betamount = findViewById<EditText>(R.id.betAmountInput).text.toString().toInt()
+
+                val headsbox = findViewById<RadioButton>(R.id.HeadsBtn)
+                val tailsbox = findViewById<RadioButton>(R.id.TailsBtn)
 
                 HeadStreak = HeadStreak + 1
                 TailStreak = 0
@@ -57,9 +64,25 @@ class CoinFlipGame : AppCompatActivity() {
                 val NextChance = findViewById<TextView>(R.id.NextChance)
                 NextChance.setText("The chances of it hitting Heads " + (HeadStreak + 1) + " times in a row is 1 in " + Integer.toString(HNextChance))
 
+                if(headsbox.isChecked && betamount > 0 && betamount < balancE){
 
-                val balanceTxt = findViewById<TextView>(R.id.balancetxt)
-                balanceTxt.setText("Balance : " + Integer.toString(balancE))
+                    balancE = (balancE + betamount)
+
+                    val balanceTxt = findViewById<TextView>(R.id.balancetxt)
+                    balanceTxt.setText("Balance : " + Integer.toString(balancE))
+                }
+
+                if(tailsbox.isChecked && betamount > 0 && betamount < balancE){
+
+                    balancE = (balancE - betamount)
+
+                    val balanceTxt = findViewById<TextView>(R.id.balancetxt)
+                    balanceTxt.setText("Balance : " + Integer.toString(balancE))
+                }
+
+                if(betamount > balancE || betamount < 0){
+                    Toast.makeText(this, "Invalid Bet Amount", Toast.LENGTH_SHORT).show()
+                }
 
                 flipCoin(R.drawable.heads, "heads")
 
@@ -68,6 +91,11 @@ class CoinFlipGame : AppCompatActivity() {
                 val newTails = CoinFlipHistory(-1,"Tails")
                 val mydatabase = DataBaseHelper(this)
                 val result = mydatabase.addCoinFlipResult(newTails)
+
+                val betamount = findViewById<EditText>(R.id.betAmountInput).text.toString().toInt()
+
+                val headsbox = findViewById<RadioButton>(R.id.HeadsBtn)
+                val tailsbox = findViewById<RadioButton>(R.id.TailsBtn)
 
                 TailStreak = TailStreak + 1
                 HeadStreak = 0
@@ -81,7 +109,25 @@ class CoinFlipGame : AppCompatActivity() {
                 val NextChance = findViewById<TextView>(R.id.NextChance)
                 NextChance.setText("The chances of it hitting Tails " + (TailStreak + 1) + " times in a row is 1 in " + Integer.toString(TNextChance))
 
-                val balanceTxt = findViewById<TextView>(R.id.balancetxt)
+                if(headsbox.isChecked && betamount > 0 && betamount < balancE){
+
+                    balancE = (balancE - betamount)
+
+                    val balanceTxt = findViewById<TextView>(R.id.balancetxt)
+                    balanceTxt.setText("Balance : " + Integer.toString(balancE))
+                }
+
+                if(tailsbox.isChecked && betamount > 0 && betamount < balancE){
+
+                    balancE = (balancE + betamount)
+
+                    val balanceTxt = findViewById<TextView>(R.id.balancetxt)
+                    balanceTxt.setText("Balance : " + Integer.toString(balancE))
+                }
+
+                if(betamount > balancE || betamount < 0){
+                    Toast.makeText(this, "Invalid Bet Amount", Toast.LENGTH_SHORT).show()
+                }
 
                 flipCoin(R.drawable.tails, "tails")
             }
